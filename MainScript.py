e8d351786c7c48443de4497b156057e69cf6fe21
Vo1d <66044267+Vc1d@users.noreply.github.com>
2020-07-26 12:00:24 -0400

    cleaned up code, added frames, changed geometry manager to grid, and added back buttons. See if you can make the grid formatting neater or more aesthetic.

--- MainScript.py
+++ MainScript.py
@@ -1,174 +1,106 @@
 from tkinter import *
 from tkinter import colorchooser
-from pygame import mixer
+import tkinter.font as tkFont
 
-win = Tk()
-win['background'] = '#A3E4D7'
-mixer.init()
-music = mixer.music.load('Ransom.mp3')
+root = Tk()
+menu = Frame(root)
+menu.grid(padx=20, pady=20)
+start = Frame(root)
+settings = Frame(root)
 
+# CONFIG
 
-musiccount = 0
-onetimeuse = 0
-mixer.init()
+fontStyle = tkFont.Font(family="Lucida Grande", size=20)
+small_font = tkFont.Font(family="Lucida Grande", size=15)
 
-
-def music_player():
-    global musiccount
-    global onetimeuse
-    if onetimeuse == 0:
-        mixer.music.play(-1)
-        onetimeuse = 1
-        musiccount = 1
-    else:
-        if musiccount == 0:
-            mixer.music.unpause()
-            musiccount = musiccount + 1
-        else:
-            mixer.music.pause()
-            musiccount = 0
+# MENU FUNCTIONS
 
 
-def start_click():
-    b_start.destroy()
-    b_settings.destroy()
-    b_quit.destroy()
-
-    def buttons_click():
-        b_button_1 = Button(win, text='Button 1')
-        b_button_1.place(x=150, y=200)
-
-    # Buttons GUI
-    b_buttons = Button(win, text='Buttons', command=buttons_click)
-    b_buttons.place(x=150, y=150)
+def open_settings():
+    menu.grid_forget()
+    settings.grid()
 
 
-def settings_click():
-    b_start.destroy()
-    b_settings.destroy()
-    b_quit.destroy()
-
-    def save_settings_click():
-        height_value = str(height_slider.get())
-        width_value = str(width_slider.get())
-        total = height_value + 'x' + width_value
-        win.geometry(total)
-
-    def click_here_click():
-        b_save_settings.destroy()
-        lb_color.destroy()
-        height_slider.destroy()
-        width_slider.destroy()
-        b_save_settings.destroy()
-
-        def click_here_save_settings_click():
-            height_entry_get = str(height_entry.get())
-            width_entry_get = str(width_entry.get())
-            win_geometry = height_entry_get + 'x' + width_entry_get
-            win.geometry(win_geometry)
-
-        # CLICK HERE ENTRIES
-        height_entry = Entry(win)
-        height_entry.place(x=210, y=150)
-
-        width_entry = Entry(win)
-        width_entry.place(x=350, y=150)
-
-        # CLICK HERE BUTTONS
-        b_click_here_save_settings = Button(win, text='Save Settings', command=click_here_save_settings_click)
-        b_click_here_save_settings.place(x=310, y=250)
-
-    def change_background_color_click():
-        b_save_settings.destroy()
-        lb_color.destroy()
-        height_slider.destroy()
-        width_slider.destroy()
-        b_save_settings.destroy()
-        b_click_here.destroy()
-        lb_height.destroy()
-        lb_width.destroy()
-        lb_click_here.destroy()
-        color_chooser = colorchooser.askcolor(title='select color')
-        win.configure(background=color_chooser[1])
-
-    def music_click():
-        b_save_settings.destroy()
-        lb_color.destroy()
-        height_slider.destroy()
-        width_slider.destroy()
-        b_save_settings.destroy()
-        b_click_here.destroy()
-        lb_height.destroy()
-        lb_width.destroy()
-        lb_click_here.destroy()
-
-        def music_save_settings_click():
-            volume_value = int(music_volume.get())
-            volume_value = volume_value / 100
-            mixer.music.set_volume(volume_value)
-
-        # MUSIC BUTTONS
-        b_music_save_settings = Button(win, text='Save Settings', command=music_save_settings_click)
-        b_music_save_settings.place(x=250, y=250)
-
-        # MUSIC LABELS
-        lb_music = Label(win, text="Music: ")
-        lb_music.place(x=260, y=100)
-
-        lb_volume = Label(win, text='Music Volume')
-        lb_volume.place(x=250, y=150)
-
-        # MUSIC SLIDERS
-        music_volume = Scale(win, from_=0, to=100, orient=HORIZONTAL)
-        music_volume.place(x=350, y=135)
-
-        # SETTINGS CHECKBOX
-        cb_music = Checkbutton(win, text='ON', command=music_player)
-        cb_music.place(x=350, y=100)
-
-    # SETTINGS BUTTONS
-    b_save_settings = Button(win, text='Save Settings', command=save_settings_click)
-    b_save_settings.place(x=310, y=300)
-
-    b_click_here = Button(win, text='Click Here', command=click_here_click)
-    b_click_here.place(x=300, y=450)
-
-    b_change_background_color = Button(win, text='Change Background Color', command=change_background_color_click)
-    b_change_background_color.place(x=250, y=50)
-
-    b_music = Button(win, text='Music', command=music_click)
-    b_music.place(x=100, y=50)
-
-    # SETTINGS LABELS
-    lb_height = Label(win, text='HEIGHT')
-    lb_height.place(x=260, y=120)
-
-    lb_width = Label(win, text='WIDTH')
-    lb_width.place(x=380, y=120)
-
-    lb_color = Label(win, text="BACKGROUND COLOR")
-    lb_color.place(x=285, y=200)
-
-    lb_click_here = Label(win, text="Or insert the window's dimensions, by clicking in the button below")
-    lb_click_here.place(x=300, y=400)
-
-    # SLIDERS
-    height_slider = Scale(win, from_=800, to=1920, orient=HORIZONTAL)
-    height_slider.place(x=230, y=150)
-
-    width_slider = Scale(win, from_=600, to=1080, orient=HORIZONTAL)
-    width_slider.place(x=350, y=150)
+def open_start():
+    menu.grid_forget()
+    start.grid()
 
 
-# General GUI
-b_start = Button(win, text='Start', command=start_click, width=10)
-b_start.place(x=350, y=250)
+def quit_prg():
+    exit()
 
-b_settings = Button(win, text='Settings', command=settings_click, width=10)
-b_settings.place(x=350, y=300)
 
-b_quit = Button(win, text='Quit', width=10)
-b_quit.place(x=350, y=350)
+# SETTINGS FUNCTIONS
 
-win.geometry('800x600')
-win.mainloop()
+
+def save_settings():
+    height_value = str(height_slider.get())
+    width_value = str(width_slider.get())
+    total = height_value + 'x' + width_value
+    root.geometry(total)
+
+
+def choose_color():
+    color_chooser = colorchooser.askcolor(title='select color')
+    root.configure(background=color_chooser[1])
+
+
+# BACK COMMANDS
+
+
+def startback():
+    start.grid_forget()
+    menu.grid()
+
+
+def settingsback():
+    settings.grid_forget()
+    menu.grid()
+
+
+# START BUTTONS
+
+b_start = Button(menu, text="Start", command=open_start, font=fontStyle)
+b_start.grid(row=0, column=0)
+
+b_test = Button(start, text="test", font=fontStyle)
+b_test.grid(row=1, column=0)
+
+start_back = Button(start, text="Back", command=startback, font=fontStyle)
+start_back.grid(row=1, column=2)
+
+# SETTINGS BUTTONS
+
+b_settings = Button(menu, text="Settings", command=open_settings, font=fontStyle)
+b_settings.grid(row=0, column=1)
+
+l_height = Label(settings, text="HEIGHT", font=small_font)
+l_height.grid(row=0, column=0)
+
+height_slider = Scale(settings, from_=800, to=1920, orient=HORIZONTAL)
+height_slider.grid(row=1, column=0)
+
+l_width = Label(settings, text="WIDTH", font=small_font)
+l_width.grid(row=0, column=1)
+
+width_slider = Scale(settings, from_=600, to=1080, orient=HORIZONTAL)
+width_slider.grid(row=1, column=1)
+
+b_save = Button(settings, text="Save", command=save_settings, font=fontStyle)
+b_save.grid(row=2, column=0)
+
+b_choose_color = Button(settings, text="Choose Background Color", command=choose_color, font=fontStyle)
+b_choose_color.grid(row=3, column=0)
+
+settings_back = Button(settings, text="Back", command=settingsback, font=fontStyle)
+settings_back.grid(row=1, column=2)
+
+# QUIT BUTTON
+
+b_quit = Button(menu, text="Quit", command=quit_prg, font=fontStyle)
+b_quit.grid(row=1, column=0, padx=50, pady=50)
+
+# END
+
+root.geometry('800x600')
+root.mainloop()
